c6b4766314c736ab90a13e8cde7eb5eae98c931d
Rob-Rychs <Robrychel@gmail.com>
2017-09-28 16:26:36 -0700

    added hello-world refactor into its own component in /components/ and then /export/import/render in app.js

---
+++ src/hello-world.js
@@ -0,0 +1,8 @@
+import './styles.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = document.getElementById('app-container');
+
+ReactDOM.render(<p>Hello, world!</p>, root);
