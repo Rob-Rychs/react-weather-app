bc3dd7cd45d3aa94da505f74c9c11b2ce83d2439
Rob-Rychs <Robrychel@gmail.com>
2017-09-28 17:33:08 -0700

    update zipForm.js

--- src/components/zipForm.js
+++ src/components/zipForm.js
@@ -1,17 +1,45 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+
 
 class ZipForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipcode: ''
+    };
+    this.inputUpdated = this.inputUpdated.bind(this);
+
+  }
+  inputUpdated(e) {
+    const {
+      value
+    } = e.target;
+
+    this.setState({
+      zipcode: value
+    });
+  }
+
   render() {
-    return (
-      <div className="zip-form">
-        <form>
-          <label htmlFor="zipcode">Zip Code</label>
-          <input
-            className="form-control"
-            type="input"
-            name="zipcode"/>
-          <button type="submit" className='btn btn-success'>Get the forecast!!</button>
-        </form>
+    return ( 
+    <div className = "zip-form">
+    <form>
+    <label htmlFor = "zipcode" > Zip Code </label>
+    <input className = "form-control"
+      type = "input"
+      name = "zipcode"
+      value = {
+        this.state.zipcode
+      }
+      onInput = {
+        this.inputUpdated
+      }/> 
+      <button type = "submit"
+      className = 'btn btn-success' > Get the forecast!! </button> 
+      </form> 
       </div>
     );
   }
