d23493aa306f74a9acc09eeefac5e5fce169c833
Rob-Rychs <Robrychel@gmail.com>
2017-10-03 06:20:16 -0700

    refactored WeatherList.js component to be a functional component

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import WeatherListItem from './WeatherListItem';
 
-class WeatherList extends Component {
+function WeatherList(props) {
+  const { days, onDayClicked } = props;
   
-  render() {
-    const { days, onDayClicked } = this.props;
-    
-    return (
-      <div className="weather-list flex-parent">
-        {days.map((day, index) =>
-          <WeatherListItem
-            key={day.dt}
-            day={day}
-            index={index}
-            onDayClicked={onDayClicked}
-          />
-        )}
-      </div>
-    );
-  }
+  return <div className="weather-list flex-parent">
+    {days.map((day, index) =>
+      <WeatherListItem
+        key={day.dt}
+        day={day}
+        index={index}
+        onDayClicked={onDayClicked}
+      />
+    )}
+  </div>; 
 }
 
 export default WeatherList;
